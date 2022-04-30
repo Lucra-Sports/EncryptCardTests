4a937a3a096f60e0ab0a70530ab1b12173dc1047
Paul Zabelin <paulz@users.noreply.github.com>
2022-04-30 02:12:19 -0700

    test valid key

--- AcceptanceTests/AcceptanceTests.swift
+++ AcceptanceTests/AcceptanceTests.swift
@@ -22,11 +22,42 @@ class AcceptanceTests: XCTestCase {
 class SwiftPGEncrypt {
     enum Error: Swift.Error {
       case invalidKey(String)
+      case invalidCertificate
     }
     
+    var keyId: String?
+    var publicKey: SecKey?
+    var subject: String?
+    var commonName: String?
+    static let padding = "***"
+    
+    func withoutPrefix(_ string: String) -> String {
+        String(string.dropFirst(Self.padding.count))
+    }
+    func withoutSuffix(_ string: String) -> String {
+        String(string.dropLast(Self.padding.count))
+    }
+
     func setKey(_ key: String) throws {
-        if !key.hasPrefix("***") || !key.hasSuffix("***") {
+        if !key.hasPrefix(Self.padding) || !key.hasSuffix(Self.padding) {
             throw Error.invalidKey("Key is not valid. Should start and end with '***'")
+        }
+        let keys = withoutPrefix(withoutSuffix(key)).components(separatedBy: .init(charactersIn: "\\|"))
+        keyId = keys[0]
+        if let data = Data(base64Encoded: keys[1]),
+           let certificate = SecCertificateCreateWithData(kCFAllocatorDefault, data as CFData),
+           let secKey = SecCertificateCopyKey(certificate),
+           SecKeyIsAlgorithmSupported(secKey, .encrypt, .rsaEncryptionRaw),
+           let summary = SecCertificateCopySubjectSummary(certificate) {
+            var name: CFString?
+            SecCertificateCopyCommonName(certificate, &name)
+            if let name = name {
+                commonName = name as String
+            }
+            publicKey = secKey
+            subject = summary as String
+        } else {
+            throw Error.invalidCertificate
         }
     }
 }
@@ -40,5 +71,17 @@ class SwiftPGEncryptTest: XCTestCase {
                 XCTFail("should be invalid key error")
             }
         }
+    }
+    
+    func testValidKey() throws {
+        let key = try! String(contentsOf: URL(fileURLWithPath: "/tmp/key.txt"))
+        let encrypt = SwiftPGEncrypt()
+        try encrypt.setKey(key)
+        XCTAssertEqual("14340", encrypt.keyId)
+        XCTAssertEqual("www.safewebservices.com", encrypt.subject)
+        XCTAssertEqual("www.safewebservices.com", encrypt.commonName)
+        XCTAssertTrue(encrypt.publicKey.debugDescription.contains(
+            "SecKeyRef algorithm id: 1, key type: RSAPublicKey, version: 4, block size: 2048 bits"
+        ))
     }
 }
