8f2b11b7e75c840ff4cacb16ebe6ce5450eda278
Paul Zabelin <paulz@users.noreply.github.com>
2022-05-01 05:54:59 -0700

    refactor away withoutPrefix

--- EncryptCard/Sources/Encrypt.swift
+++ EncryptCard/Sources/Encrypt.swift
@@ -27,13 +27,6 @@ public class Encrypt {
     static let format = "GWSC"
     static let version = "1"
     
-    func withoutPrefix(_ string: String) -> String {
-        String(string.dropFirst(Self.padding.count))
-    }
-    func withoutSuffix(_ string: String) -> String {
-        String(string.dropLast(Self.padding.count))
-    }
-    
     public func encrypt(_ string: String) throws -> String {
         guard let publicKey = publicKey, let keyId = keyId else {
             throw Error.invalidKey("key is not set, unable to encrypt")
@@ -71,7 +64,7 @@ public class Encrypt {
         if !key.hasPrefix(Self.padding) || !key.hasSuffix(Self.padding) {
             throw Error.invalidKey("Key is not valid. Should start and end with '***'")
         }
-        let keys = withoutPrefix(withoutSuffix(key))
+        let keys = key.trimmingCharacters(in: .init(charactersIn: "*"))
             .components(separatedBy: .init(charactersIn: "\\|"))
         keyId = keys.first!
         
