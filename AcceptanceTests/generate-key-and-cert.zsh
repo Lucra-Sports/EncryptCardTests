4dad07e8a34b165c9542fd5db5588a2c54299815
Paul Zabelin <paulz@users.noreply.github.com>
2022-04-30 21:58:19 -0700

    update config file to match existing certificate

--- AcceptanceTests/generate-key-and-cert.zsh
+++ AcceptanceTests/generate-key-and-cert.zsh
@@ -8,7 +8,7 @@
 here=${0:a:h}
 which openssl
 openssl version
-openssl req -x509 -text \
+openssl req -x509 \
     -nodes -config ${here}/openssl-config.txt \
     -newkey rsa:2048 -set_serial 0 \
     -keyout /tmp/example.key \
