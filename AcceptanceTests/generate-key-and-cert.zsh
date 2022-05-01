7917b0509b477cb1f5fea4c0e52c09a14d8beb84
Paul Zabelin <paulz@users.noreply.github.com>
2022-04-30 22:18:57 -0700

    add example certificate and private key

--- AcceptanceTests/generate-key-and-cert.zsh
+++ AcceptanceTests/generate-key-and-cert.zsh
@@ -5,11 +5,12 @@
 #
 #  Created by Paul Zabelin on 4/30/22.
 #  
-here=${0:a:h}
+pushd ${0:a:h}
 which openssl
 openssl version
 openssl req -x509 \
-    -nodes -config ${here}/openssl-config.txt \
+    -nodes -config ./openssl-config.txt \
     -newkey rsa:2048 -set_serial 0 \
-    -keyout /tmp/example.key \
-    -out /tmp/example.pem
+    -keyout ./example-private-key.txt \
+    -out ./example-certificate.pem
+popd
