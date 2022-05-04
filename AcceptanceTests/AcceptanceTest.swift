9753f527992ebdc9544e94ce910137835a11e9be
Paul Zabelin <paulz@users.noreply.github.com>
2022-05-04 03:48:03 -0700

    test outputs are equivalent, same except RSA encrypts AES keys with random padding

--- AcceptanceTests/AcceptanceTest.swift
+++ AcceptanceTests/AcceptanceTest.swift
@@ -27,6 +27,67 @@ class AcceptanceTest: XCTestCase {
             return try encrypt.encrypt(creditCard: card)
         }
     }
+    func testSameOutputForSameSeedExceptAesKeysThatWereRSAencrypted() throws {
+        let card = CreditCard(
+            cardNumber: "4111111111111111",
+            expirationDate: "10/25",
+            cvv: "123"
+        )
+        let pgCard = PGKeyedCard(cardNumber: card.cardNumber,
+                               expirationDate: card.expirationDate,
+                               cvv: card.cvv)
+
+        let oldEncrypt = PGEncrypt()
+        let keyUrl = try keysUrl(file: "example-payment-gateway-key.txt")
+        let key = try String(contentsOf: keyUrl)
+        oldEncrypt.setKey(key)
+        let oldOutput = try XCTUnwrap(oldEncrypt.encrypt(pgCard, includeCVV: true))
+        let oldOutput2 = try XCTUnwrap(oldEncrypt.encrypt(pgCard, includeCVV: true))
+        XCTAssertNotEqual(oldOutput, oldOutput2,
+                          "same data encrypted produces different output")
+
+        let pemUrl = try keysUrl(file: "example-private-key.txt")
+        let permString = try XCTUnwrap(String(contentsOf: pemUrl))
+        let privateKey = try PrivateKey(pemEncoded: permString)
+        XCTAssertNotNil(privateKey)
+        
+        let decodedData = try XCTUnwrap(Data(base64Encoded: oldOutput))
+        let decodedString = try XCTUnwrap(String(data: decodedData, encoding: .ascii))
+        let oldComponents = decodedString.components(separatedBy: "|")
+        let aesEncryptedKeyData = try XCTUnwrap(Data(base64Encoded: oldComponents[3]))
+        let ivData = try XCTUnwrap(Data(base64Encoded: oldComponents[4]))
+        XCTAssertEqual(ivData.count, 16)
+        let message = EncryptedMessage(data: aesEncryptedKeyData)
+        let oldAesKeyData = try message.decrypted(with: privateKey, padding: .PKCS1).data
+
+        let newEncrypt = try EncryptCard(key: key)
+        newEncrypt.randomFunction = {
+            [
+                16: ivData,
+                32: oldAesKeyData
+            ][$0]!
+        }
+        let newOutput = try newEncrypt.encrypt(creditCard: card)
+        XCTAssertEqual(try decrypt(base64: newOutput), try decrypt(base64: oldOutput),
+        "decrypted outout is the same")
+        XCTAssertNotEqual(newOutput, oldOutput, "different output due to random padding")
+        let newDecoded = try XCTUnwrap(String(
+            data: try XCTUnwrap(Data(base64Encoded: newOutput)), encoding: .ascii)
+        )
+        let newComponents = newDecoded.components(separatedBy: "|")
+        for (index, oldComponent) in oldComponents.enumerated() {
+            if index == 3 {
+                XCTAssertNotEqual(newComponents[index], oldComponent, "encrypted AES keys")
+            } else {
+                XCTAssertEqual(newComponents[index], oldComponent, "other data is the same")
+            }
+        }
+        let newAesKeyEncrypted = try XCTUnwrap(Data(base64Encoded: newComponents[3]))
+        let newMessage = EncryptedMessage(data: newAesKeyEncrypted)
+        let newAesKeyData = try newMessage.decrypted(with: privateKey, padding: .PKCS1).data
+        XCTAssertEqual(newAesKeyData, oldAesKeyData,
+                       "same AES key encrypted differently by RSA")
+    }
     
     func testCertificate() throws {
         let cerUrl = try keysUrl(file: "example-certificate.cer")
@@ -77,6 +138,7 @@ class AcceptanceTest: XCTestCase {
 
         let message = EncryptedMessage(data: aesEncryptedKeyData)
         let aesKeyData = try message.decrypted(with: privateKey, padding: .PKCS1).data
+        XCTAssertEqual(aesKeyData.count, 32)
         let cypher = try AES(key: aesKeyData.bytes, blockMode: CBC(iv: ivData.bytes), padding: .pkcs5)
         let decryptedCard = try cypher.decrypt(cardData.bytes)
         return try XCTUnwrap(String(data: Data(decryptedCard), encoding: .ascii))
